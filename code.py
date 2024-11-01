1b546b945f0ac5d1baefcfb61b01263842d5fc0c
elban <elbankawtar@gmail.com>
2024-11-01 15:03:23 +0100

    pdf change

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
-
-
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 with st.sidebar :
     st.title(' llm chat app ')
@@ -22,6 +22,23 @@ def main():
     st.write("chto PDF")
     
     pdf=st.file_uploader("Upload your PDF", type='pdf')
+
+    #st.write(pdf)
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+        
+
+        text_splitter=RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        #st.write(text)
 
 
 
