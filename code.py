d36bfc51d16abbdb6809f6abe2a2049f7fac004c
elban <elbankawtar@gmail.com>
2024-11-02 21:36:05 +0100

    'faiss'

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 with st.sidebar :
     st.title(' llm chat app ')
@@ -37,6 +39,13 @@ def main():
             chunk_overlap=200,
             length_function=len
         )
+        chunks=text_splitter.split_text(text=text)
+
+        #Embeddings
+        embeddings = OpenAIEmbeddings() 
+        VectorStore =FAISS.from_texts(chunks, embeddings=embeddings)
+
+    
 
         #st.write(text)
 
